c80240fc983a78476077e3b46ab2d254cd72eb66
Inaeah <nayahsaffanah@gmail.com>
2025-11-24 14:35:58 +0700

    create models and card for nobar spot

---
+++ lib/homepage/models/spot_entry.dart
@@ -0,0 +1,61 @@
+// To parse this JSON data, do
+//
+//     final spotEntry = spotEntryFromJson(jsonString);
+
+import 'dart:convert';
+
+List<SpotEntry> spotEntryFromJson(String str) => List<SpotEntry>.from(json.decode(str).map((x) => SpotEntry.fromJson(x)));
+
+String spotEntryToJson(List<SpotEntry> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class SpotEntry {
+    String id;
+    String name;
+    String thumbnail;
+    String homeTeam;
+    String awayTeam;
+    DateTime date;
+    String time;
+    String city;
+    String address;
+    int host;
+
+    SpotEntry({
+        required this.id,
+        required this.name,
+        required this.thumbnail,
+        required this.homeTeam,
+        required this.awayTeam,
+        required this.date,
+        required this.time,
+        required this.city,
+        required this.address,
+        required this.host,
+    });
+
+    factory SpotEntry.fromJson(Map<String, dynamic> json) => SpotEntry(
+        id: json["id"],
+        name: json["name"],
+        thumbnail: json["thumbnail"] ?? "",
+        homeTeam: json["home_team"],
+        awayTeam: json["away_team"],
+        date: DateTime.parse(json["date"]),
+        time: json["time"],
+        city: json["city"],
+        address: json["address"],
+        host: json["host"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "thumbnail": thumbnail,
+        "home_team": homeTeam,
+        "away_team": awayTeam,
+        "date": "${date.year.toString().padLeft(4, '0')}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}",
+        "time": time,
+        "city": city,
+        "address": address,
+        "host": host,
+    };
+}
---
+++ lib/homepage/widget/spot_entry_card.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import '../models/spot_entry.dart';
+
+class SpotEntryCard extends StatelessWidget {
+  final SpotEntry spot;
+  final VoidCallback onTap;
+
+  const SpotEntryCard({
+    super.key,
+    required this.spot,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      child: InkWell(
+        onTap: onTap,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            side: BorderSide(color: Colors.grey.shade300),
+          ),
+          elevation: 2,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Thumbnail
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(6),
+                  child: Image.network(
+                    'http://localhost:8000/proxy-image/?url=${Uri.encodeComponent(spot.thumbnail)}',
+                    height: 150,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) => Container(
+                      height: 150,
+                      color: Colors.grey[300],
+                      child: const Center(child: Icon(Icons.broken_image)),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+
+                // Name
+                Text(
+                  spot.name,
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 6),
+
+                // City
+                Text(
+                  spot.city,
+                  style: const TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                const SizedBox(height: 6),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
