1d3819dc1935275bcf195d69dd01868eacfa666b
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2025-12-01 13:59:38 +0700

    implements account page for admin

--- lib/account/screens/admin_account_page.dart
+++ lib/account/screens/admin_account_page.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:nobarpedia_mobile/account/widget/user_card.dart';
+import 'package:nobarpedia_mobile/config.dart';
 import 'package:nobarpedia_mobile/widgets/left_drawer.dart';
-// import 'package:nobarpedia_mobile/widgets/product_card.dart';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
 
-class AdminAccountPage extends StatelessWidget {
+class AdminAccountPage extends StatefulWidget {
   const AdminAccountPage({super.key});
 
   @override
+  State<AdminAccountPage> createState() => _AdminAccountPage();
+}
+
+class _AdminAccountPage extends State<AdminAccountPage> {
+  Future<List<dynamic>> fetchAllUser(CookieRequest request) async {
+    final response = await request.get('$baseUrl/account/api/admin/');
+
+    // tiap user punya: is_admin, id, username
+    List<dynamic> data = response['responseData'];
+    return data;
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
     return Scaffold(
       // AppBar adalah bagian atas halaman yang menampilkan judul.
@@ -14,14 +31,20 @@ class AdminAccountPage extends StatelessWidget {
         // Judul aplikasi dengan teks putih dan tebal.
         title: RichText(
           text: TextSpan(
-            style: const TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.w600,
-            ),
+            style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
             children: <TextSpan>[
-              TextSpan(text: 'Nobar', style: TextStyle(color: Colors.white)),
-              TextSpan(text: 'Pedia', style: TextStyle(color: Colors.green)),
-              TextSpan(text: ' Admin Page', style: TextStyle(color: Colors.white)),
+              TextSpan(
+                text: 'Nobar',
+                style: TextStyle(color: Colors.white),
+              ),
+              TextSpan(
+                text: 'Pedia',
+                style: TextStyle(color: Colors.green),
+              ),
+              TextSpan(
+                text: ' Admin Page',
+                style: TextStyle(color: Colors.white),
+              ),
             ],
           ),
         ),
@@ -29,17 +52,29 @@ class AdminAccountPage extends StatelessWidget {
       ),
       drawer: LeftDrawer(),
       // Body halaman dengan padding di sekelilingnya.
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        // Menyusun widget secara vertikal dalam sebuah kolom.
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-
-            // isi
-
-          ],
-        ),
+      backgroundColor: const Color(0xFF1E1E1E), 
+      body: FutureBuilder(
+        future: fetchAllUser(request),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return ListView.builder(
+              padding: const EdgeInsets.only(top: 20),
+              itemCount: snapshot.data!.length,
+              itemBuilder: (_, index) {
+                return UserCard(
+                  userData: snapshot.data![index], 
+                  request: request,
+                  // Refresh page
+                  onRefresh: () {
+                    setState(() {}); 
+                  },
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
